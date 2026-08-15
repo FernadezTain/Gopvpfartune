1024676f2daf12f8a73c0d18e3b2bf4e03101946
FernadezTain <fernadezofficial489@gmail.com>
2026-08-15 13:17:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-window.API_BASE = "";
+window.AVIATOR_API_BASE = "http://a46466-b92d.y.d-f.pw/:8090";
+window.SUPABASE_URL = "https://ptpxuujqlarucsexbjgt.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cHh1dWpxbGFydWNzZXhiamd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2ODM2NzgsImV4cCI6MjEwMjI1OTY3OH0.h5fQ4u2a10NYQAst2QFuyfDiIKYRPwf5tr_VBl1Em6o"; // ЭТОТ, не service_role!
