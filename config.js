d900f6977c064447e94a245d81f55cf235359ba4
FernadezTain <fernadezofficial489@gmail.com>
2026-08-20 20:14:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
 window.API_BASE = "https://gopvp-fartune.vercel.app";
+window.AVIATOR_API_BASE = "https://a46466-b92d.y.d-f.pw";
+window.SUPABASE_URL = "https://ptpxuujqlarucsexbjgt.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0cHh1dWpxbGFydWNzZXhiamd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2ODM2NzgsImV4cCI6MjEwMjI1OTY3OH0.h5fQ4u2a10NYQAst2QFuyfDiIKYRPwf5tr_VBl1Em6o";
